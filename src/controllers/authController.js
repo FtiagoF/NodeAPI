35046f18c18c886b293a1f01aa6ab8e231a5c6c1
Tiago Farias <tiagofariasnh19@gmail.com>
2021-10-29 20:14:08 -0300

    Authenticate control

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,32 +1,56 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const authConfig = require("../config/auth");
 
 const router = express.Router();
 
-router.get('/users', async(req, res) => {
-    try{
-        const users = await User.find();
-        return res.json({users});
-    }catch(err){
-        return res.json({err});
-    }
-    
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.json({ users });
+  } catch (err) {
+    return res.json({ err });
+  }
 });
 
-router.post('/register', async (req, res) => {
-    const { email } = req.body;
+router.post("/register", async (req, res) => {
+  const { email } = req.body;
 
-    try {
-        if(await User.findOne({ email }))
-            return res.status(400).send({ erro: 'User already exists' });
+  try {
+    if (await User.findOne({ email }))
+      return res.status(400).send({ erro: "User already exists" });
 
-        const user = await User.create(req.body);
+    const user = await User.create(req.body);
 
-        user.password = undefined;
-        return res.send({ user });
-    } catch(err){
-        return res.status(400).send(err);
-    }
+    user.password = undefined;
+    return res.send({ user, token: generateToken({ id: user.id }) });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 });
 
-module.exports = app => app.use('/auth', router);
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) return res.status(400).send({ error: "User not found" });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: "Invalid password" });
+
+  user.password = undefined;
+
+  res.send({ user, token: generateToken({ id: user.id }) });
+});
+
+module.exports = (app) => app.use("/auth", router);
